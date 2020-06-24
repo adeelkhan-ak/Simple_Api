340221bba26ad8f584b2ee33a7cf8ae78d83a4a9
adeelkhan-ak <ak140175@gmail.com>
2020-06-24 12:29:40 -0700

    student dic

---
+++ api.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+app = Flask(__name__)
+api = Api(app)
+
+STUDENTS = {
+    '1': {'name':'mark', 'age':23, 'spec':'math'},
+    '2': {'name': 'Jane', 'age': 20, 'spec': 'biology'},
+    '3': {'name': 'Peter', 'age': 21, 'spec': 'history'},
+    '4': {'name': 'Kate', 'age': 22, 'spec': 'science'},
+}
+
+if __name__ == "__main__":
+  app.run(debug=True)
