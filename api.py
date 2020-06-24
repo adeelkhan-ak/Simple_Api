730ee627ca48b54871dcf0049a755a8bcd545d07
adeelkhan-ak <ak140175@gmail.com>
2020-06-24 12:42:44 -0700

    improvement in identation

--- api.py
+++ api.py
@@ -9,10 +9,26 @@ STUDENTS = {
     '3': {'name': 'Peter', 'age': 21, 'spec': 'history'},
     '4': {'name': 'Kate', 'age': 22, 'spec': 'science'},
 }
+parser = reqparse.RequestParser()
 class StudentsList(Resource):
-  def get(self);
-  def post(self):
+    def get(self):
+        return STUDENTS
+  
+    def post(self):
+        parser.add_argument("name")
+        parser.add_argument("age")
+        parser.add_argument("spec")
+        args = parser.parse_args()
+        student_id = int(max(STUDENTS.keys())) + 1
+        student_id = '%i' % student_id
+        STUDENTS[student_id] = {
+            "name": args["name"],
+            "age": args["age"],
+            "spec": args["spec"],
+            }
+        return STUDENTS[student_id], 201
+
 
 api.add_resource(StudentsList, '/students/')  
 if __name__ == "__main__":
-  app.run(debug=True)
+   app.run(debug=True)
